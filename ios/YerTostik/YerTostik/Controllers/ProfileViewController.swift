1d7dec100ad91301512dcc64c7bfce6deba695b9
aitemr <aitemr16@gmail.com>
2018-03-25 02:11:41 +0600

    feat(ProfileViewController): add settings button

--- ios/YerTostik/YerTostik/Controllers/ProfileViewController.swift
+++ ios/YerTostik/YerTostik/Controllers/ProfileViewController.swift
@@ -7,13 +7,42 @@
 //
 
 import UIKit
+import EasyPeasy
 
 class ProfileBoardViewController: UIViewController {
     
+    // MARK: View LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigationBar()
+        configureViews()
+        configureConstraints()
         self.view.backgroundColor = .white
         self.navigationItem.title = "Профиль"
+    }
+    
+    // MARK: Configure Navigation Bar
+    
+    func configureNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "settings"), style: .plain, target: self, action: #selector(settingsButtonDidPress))
+    }
+    
+    // MARK: Configure Views
+    
+    func configureViews() {
+
+    }
+    
+    // MARK: Configure Constraints
+    
+    func configureConstraints() {
+    }
+    
+    // MARK: User Interaction
+    
+    @objc func settingsButtonDidPress() {
+        
     }
     
 }
